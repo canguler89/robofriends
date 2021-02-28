cb184e7d1d4bed0f92c869eeba0b942521677d4d
Can Guler <canmuhammetguler@gmail.com>
2021-02-27 22:07:06 -0600

    searchfield created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from "react"
 import './App.css';
 import CardList from "./CardList"
+import SearchBox from './SearchBox'
 import { robots } from "./robots"
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+    this.state ={
+      robots: robots,
+      searchField: ''
+    }
+  }
+  onSearchChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+
+  render(){
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    })
+  
   return (
-      <CardList robots={robots} />
+      <div className="tc">
+        <h1>Robot Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
   );
-}
+}}
 
 export default App;
