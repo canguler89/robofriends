493c51873f130cd6816e9c58e340887600ffc3e2
Can Guler <canmuhammetguler@gmail.com>
2021-02-27 21:22:19 -0600

    rendered the robots list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Card from "./Card"
+import CardList from "./CardList"
 import { robots } from "./robots"
 
 function App() {
   return (
-    <div className="App">
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+      <CardList robots={robots} />
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({name, id, email}) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow' >
-            <img src={`https://robohash.org/${props.id}?size=200x200`} alt="robot"/>
+            <img src={`https://robohash.org/${id}?size=200x200`} alt="robot"/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
